79160389ae6950049f9e098b538174cf010a02da
Alex Mickelson <alexmickelson96@gmail.com>
2025-06-25 21:42:04 +0000

    working on tailwind

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import "bootstrap/scss/bootstrap.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { App } from "./App";
 import { WebSocketProvider } from "./contexts/WebSocketContext";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
