8eaafa1e291ca38972fffda00c09233d3f818cd1
Alex Mickelson <alexmickelson96@gmail.com>
2025-06-26 22:12:35 +0000

    secret

--- run.sh
+++ run.sh
@@ -6,11 +6,10 @@ docker build -t discord-bot .
 # docker run -it --rm discord-bot
 
 
-echo $DISCORD_SECRET
 docker rm -f discord-bot || true
 docker run -d \
   --name discord-bot \
-  -e DISCORD_SECRET=$DISCORD_SECRET \
+  --env-file .env \
   --restart always \
   -p 0.0.0.0:5677:5677 \
   -p 0.0.0.0:5678:5678 \
