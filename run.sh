6d891de31d66a7ca5c5be6178a06661fc774f536
Alex Mickelson <alexmickelson96@gmail.com>
2025-06-26 22:27:54 +0000

    compose

--- run.sh
+++ run.sh
@@ -3,15 +3,4 @@
 git pull
 docker pull node:20
 docker pull python:3.12
-docker build -t discord-bot .
-# docker run -it --rm discord-bot
-
-
-docker rm -f discord-bot || true
-docker run -d \
-  --name discord-bot \
-  --env-file .env \
-  --restart always \
-  -p 0.0.0.0:5677:5677 \
-  -p 0.0.0.0:5678:5678 \
-  discord-bot
+docker compose up --build -d
