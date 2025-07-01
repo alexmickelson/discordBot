f0f441c9e3526256be51e785523bfea06ba97da8
Alex Mickelson <alexmickelson96@gmail.com>
2025-07-01 18:07:14 +0000

    bottom bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,15 +6,21 @@ import { SongQueue } from "./features/songQueue/SongQueue";
 
 export const App = () => {
   return (
-    <div className="max-w-4xl mx-auto mt-12">
-      <h1 className="text-center text-3xl font-bold mb-8">Discord Music</h1>
-      <CurrentSong />
-      <SongQueue />
-      <PlaybackInfo />
-      <hr />
-      <AllSongs />
-      <hr />
-      <AddUrlToQueue />
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-1 max-w-4xl mx-auto w-full mt-12 mb-32">
+        <h1 className="text-center text-3xl font-bold mb-8">Discord Music</h1>
+        <SongQueue />
+        <PlaybackInfo />
+        <hr />
+        <AllSongs />
+        <hr />
+        <AddUrlToQueue />
+      </div>
+      <div className="fixed bottom-0 left-0 w-full z-50 bg-gray-900 bg-opacity-90 border-t border-slate-600">
+        <div className="max-w-4xl mx-auto w-full">
+          <CurrentSong />
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/features/currentlyPlaying/CurrentSong.tsx
+++ client/src/features/currentlyPlaying/CurrentSong.tsx
@@ -1,28 +1,38 @@
 import { useMusicWebSocket } from "../../contexts/useMusicWebSocketContexts";
 import { useWebSocketConnection } from "../../contexts/useWebSocket";
 import { Slider } from "./Slider";
+import { Spinner } from "../../utils/Spinner";
 
 export const CurrentSong = () => {
   const { playbackInfo, sendMessage } = useMusicWebSocket();
   const { ws } = useWebSocketConnection();
   return (
     <>
-      {playbackInfo && (
-        <div className="rounded-sm border p-3 my-5 bg-gray-900 bg-opacity-50">
-          <h2 className="text-xl font-semibold mb-2">Playing Song</h2>
-          <h5 className="text-lg font-medium mb-4">{playbackInfo.file_name}</h5>
-          {ws && (
-            <Slider
-              min={0}
-              max={playbackInfo.duration}
-              current={playbackInfo.current_position}
-              onChange={(v) => {
-                sendMessage({ action: "seek_to_position", position: v });
-              }}
-            />
-          )}
-        </div>
-      )}
+      <div className=" bg-gray-900 bg-opacity-50 flex flex-col items-center justify-center min-h-[80px]">
+        {playbackInfo ? (
+          <div>
+            <h2 className="text-xl font-semibold mb-2">Playing Song</h2>
+            <h5 className="text-lg font-medium mb-4">
+              {playbackInfo.file_name}
+            </h5>
+            {ws && (
+              <Slider
+                min={0}
+                max={playbackInfo.duration}
+                current={playbackInfo.current_position}
+                onChange={(v) => {
+                  sendMessage({ action: "seek_to_position", position: v });
+                }}
+              />
+            )}
+          </div>
+        ) : (
+          <>
+            <Spinner />
+            <div className="text-gray-400">No song is currently playing</div>
+          </>
+        )}
+      </div>
     </>
   );
 };
